3c059ae81f5adf72a4b83dcf29bdecb102f71115
Stephany Paulino Balbi <stephany_paulino1@ucne.edu.do>
2022-06-27 14:17:37 -0400

    Creando base del proyecto

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using DAL;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);//Recibe parametros de la consola
 
 // Add services to the container.
 
@@ -12,16 +12,16 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<Contexto>(options =>  options.UseSqlite(ConStr));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();//Explorar la app
+builder.Services.AddSwaggerGen();//Documentacion de API
 
-var app = builder.Build();
+var app = builder.Build();//Se construya la app 
 
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
 //{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwagger();//Para que el swagger se use
+    app.UseSwaggerUI();// Para que el usuario
 //}
 
 app.UseHttpsRedirection();
